8f16f6f5ff05fbe9bb42455810119253c855e8b0
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-04-08 17:08:46 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,11 +4,19 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+    const stocks = this.props.stocks.map(stockObj => {
+      return (
+        <Stock
+          key={stockObj.id}
+          stock={stockObj}
+          clickHandler={this.props.clickHandler}
+        />)
+    })
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            stocks
           }
       </div>
     );
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,20 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+    const stocks = this.props.stocks.map(stockObj => {
+      return (
+        <Stock
+          key={stockObj.id}
+          stock={stockObj}
+          clickHandler={this.props.clickHandler}
+        />)
+    })
+
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          stocks
         }
       </div>
     );
